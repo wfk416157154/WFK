4f575c4086a3c56878fafbeacdd025b854374bad
wfk416157154 <110164605+wfk416157154@users.noreply.github.com>
2022-08-01 16:27:23 +0800

    Update Hello.java

--- user-wfk/src/main/java/cn/kgc/Hello.java
+++ user-wfk/src/main/java/cn/kgc/Hello.java
@@ -2,7 +2,7 @@ package cn.kgc;
 
 public class Hello {
     public static void main(String[] args) {
-        //111
+        //111222
         System.out.println("Hello");
         System.out.println("再次Hello");
 
